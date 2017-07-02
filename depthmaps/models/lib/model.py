d3ab89493264bbc918716e493b470b4c7f04629a
Alexander Höreth <ahoereth@gmail.com>
2017-07-02 16:39:28 +0200

    Implement logging functionality in base model.

--- depthmaps/models/lib/model.py
+++ depthmaps/models/lib/model.py
@@ -1,26 +1,29 @@
+"""Depth map generation base model."""
 from collections import namedtuple
+from datetime import datetime
+from pathlib import Path
 from queue import Queue, Empty
 from threading import Thread
 
 import tensorflow as tf
 
+from .utils import to_float
 from .dataflow import Dataflow
 
 
 Network = namedtuple('Network', ('output', 'train', 'loss'))
 
 
-def to_float(images):
-    """Convert uint8 images to float and scale them from -1 to 1."""
-    return (tf.image.convert_image_dtype(images, tf.float32) - .5) * 2
-
-
 class Model:
+    """Depth map generation base model."""
     input_shape = (0, 0, 0)
     target_shape = (0, 0, 0)
     batchsize = 32
 
     def __init__(self, dataset, workers=1, checkpoint=None):
+        time = datetime.now().strftime('%y%m%d-%H%M')
+        self.logdir = Path('logs') / type(self).__name__ / time
+
         self.dataset = dataset
         self.session = tf.Session()
 
@@ -49,12 +52,18 @@ class Model:
         self.train_net = build_network(train_inputs, train_targets)
         self.test_net = build_network(test_inputs, test_targets)
 
+        # Save the model regularily and maybe restore a saved model.
         self.saver = tf.train.Saver(max_to_keep=1,
                                     keep_checkpoint_every_n_hours=1)
         if checkpoint is not None:
             self.saver.restore(checkpoint)
         else:
             self.session.run(tf.global_variables_initializer())
+
+        # Store summaries for tensorboard.
+        self.summaries = tf.summary.merge_all()
+        self.writer = tf.summary.FileWriter(Path('logs') / self.logdir,
+                                            self.session.graph)
 
         self.train_queue = Queue(len(self.dataset.train_files))
         self.workers = [Thread(target=self.train_worker, daemon=True)
@@ -85,6 +94,11 @@ class Model:
                     [self.train_net.train, self.train_net.loss],
                     {self.training: True})
                 print('Epoch {} with train loss {:.5f}.'.format(epoch, loss))
+            elif task == 'train+save':
+                summary, _ = self.session.run(
+                    [self.summaries, self.train_net.train],
+                    {self.training: True})
+                self.writer.add_summary(summary)
             else:
                 self.session.run(self.train_net.train, {self.training: True})
 
@@ -96,9 +110,13 @@ class Model:
             worker.start()
 
         # Allow a specific amount of training steps with regular evaluations.
+        steps = len(self.dataset.train_files) // self.batchsize
         for epoch in range(1, epochs + 1):
-            for _ in range(len(self.dataset.train_files) // self.batchsize):
-                self.train_queue.put((epoch, 'train'))
+            for i in range(steps, -1, -1):
+                if i == 0:
+                    self.train_queue.put((epoch, 'train+save'))
+                else:
+                    self.train_queue.put((epoch, 'train'))
             if test_frequency > 0 and not epoch % test_frequency:
                 if test_on_testset:
                     self.train_queue.put((epoch, 'evaluate'))
--- depthmaps/models/lib/utils.py
+++ depthmaps/models/lib/utils.py
@@ -1,3 +1,4 @@
+"""Utility functions."""
 import tensorflow as tf
 
 
@@ -12,3 +13,8 @@ def soft_labels_like(like, value: bool):
         return tf.random_uniform(tf.shape(like), 0.7, 1.2, tf.float32)
     else:
         return tf.random_uniform(tf.shape(like), 0., 0.3, tf.float32)
+
+
+def to_float(images):
+    """Convert uint8 images to float and scale them from -1 to 1."""
+    return (tf.image.convert_image_dtype(images, tf.float32) - .5) * 2
