65e3c678e1d31f9340cc4e63673cccab77547fca
Alexander Höreth <a.hoereth@gmail.com>
2017-09-24 12:44:56 +0200

    Scale all the things.

--- models/multiscale.py
+++ models/multiscale.py
@@ -24,6 +24,11 @@ class MultiScale(Model):
         Note: inputs and targets are expected to be scaled from 0 to 1 when
         being passed to this network.
         """
+        inputs = tf.subtract(inputs * 2, 1., name='scaled_inputs')
+        targets = tf.subtract(targets * 2, 1., name='scaled_targets')
+        tf.summary.histogram('input', inputs)
+        tf.summary.histogram('target', targets)
+
         with tf.variable_scope('coarse'):
             coarse = tf.layers.conv2d(inputs, 96, 11, activation=tf.nn.relu,
                                       strides=4)
@@ -49,14 +54,14 @@ class MultiScale(Model):
             fine = tf.concat([fine, coarse], 3)  # Coarse results enter!!
             fine = tf.layers.conv2d(fine, 64, 5, activation=tf.nn.relu,
                                     padding='same')
-            # Using sigmoid here which the paper does not because the targets
-            # are scaled from 0 to 1 the same way the inputs are.
             outputs = tf.layers.conv2d(fine, 1, 5, padding='same',
-                                       activation=tf.nn.sigmoid)
+                                       activation=tf.nn.tanh)
 
         loss = tf.losses.mean_squared_error(targets, outputs)
         global_step = tf.train.get_or_create_global_step()
         optimizer = tf.train.AdamOptimizer(1e-4)
         train_op = optimizer.minimize(loss, global_step)
 
+        # Scale outputs back to between 0 and 1
+        outputs = tf.divide(outputs + 1., 2., name='outputs')
         return outputs, train_op, [loss]
--- models/simple.py
+++ models/simple.py
@@ -14,16 +14,22 @@ class Simple(Model):
         Note: inputs and targets are expected to be scaled from 0 to 1 when
         being passed to this network.
         """
+        inputs = tf.subtract(inputs * 2, 1., name='scaled_inputs')
+        targets = tf.subtract(targets * 2, 1., name='scaled_targets')
+        tf.summary.histogram('input', inputs)
+        tf.summary.histogram('target', targets)
+
         with tf.variable_scope('network'):
             net = tf.layers.conv2d(inputs, 16, 16, 2, activation=tf.nn.relu)
-            net = tf.layers.conv2d(net, 16, 8, 1, activation=tf.nn.relu)
-            net = tf.layers.conv2d(net, 32, 4, 1, activation=tf.nn.relu)
-            # Using sigmoid to scale output images from 0 to 1.
-            outputs = tf.layers.conv2d(net, 1, 1, activation=tf.nn.sigmoid)
+            net = tf.layers.conv2d(net, 32, 8, 1, activation=tf.nn.relu)
+            net = tf.layers.conv2d(net, 48, 4, 1, activation=tf.nn.relu)
+            net = tf.layers.conv2d(net, 1, 1, activation=tf.nn.tanh)
 
         global_step = tf.train.get_or_create_global_step()
-        loss = tf.losses.mean_squared_error(targets, outputs)
-        optimizer = tf.train.AdamOptimizer()
+        loss = tf.losses.mean_squared_error(targets, net)
+        optimizer = tf.train.AdamOptimizer(1e-4)
         train_op = optimizer.minimize(loss, global_step)
 
+        # Scale outputs back to between 0 and 1
+        outputs = tf.divide(net + 1., 2., name='outputs')
         return outputs, train_op, [loss]
